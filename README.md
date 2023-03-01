0f0011df5c87febc976f9665fc6c5ba8a34c5dd4
plasticine <975036719@qq.com>
2023-03-01 23:52:29 +0800

    feat(cli): mvp cli

---
+++ README.md
@@ -0,0 +1,7 @@
+# plasticine-islands
+
+基于 [islands 架构](https://www.patterns.dev/posts/islands-architecture/) 实现的 SSG 框架
+
+## Features
+
+## Usage
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,23 @@
-console.log('init')
+import cac from 'cac'
+
+import pkg from '../package.json'
+
+const cli = cac('plasticine-islands')
+
+// build 命令
+cli.command('build [root]', '构建产物').action((root?: string) => {
+  console.log(`build ${root}`)
+})
+
+// dev 命令
+cli.command('dev [root]', '启动开发环境服务器').action((root?: string) => {
+  console.log(`dev ${root}`)
+})
+
+// 支持 `--help` 和 `-h` 显示帮助信息
+cli.help()
+
+// 支持 `--version` 和 `-v` 显示版本信息
+cli.version(pkg.version)
+
+cli.parse()
