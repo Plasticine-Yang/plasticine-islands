615c9e8fdeca8791717918c01579f6f072d9a203
plasticine <975036719@qq.com>
2023-03-10 00:35:56 +0800

    feat(cli): actionDev 集成 core 包提供的 createDevServer

--- packages/cli/src/actions/dev.ts
+++ packages/cli/src/actions/dev.ts
@@ -1,5 +1,15 @@
+import { resolve } from 'path'
+
+import { createDevServer } from '@plasticine-islands/core'
 import type { ActionDevFunc } from '@plasticine-islands/types'
 
-export const actionDev: ActionDevFunc = (root) => {
-  console.log(`dev ${root}`)
+/** @inheritdoc */
+export const actionDev: ActionDevFunc = async (root) => {
+  /** @description 需要将相对路径 root 解析成绝对路径，默认使用命令执行时的路径作为 root */
+  const parsedRoot = root !== undefined ? resolve(root) : process.cwd()
+
+  const server = await createDevServer({ root: parsedRoot })
+  await server.listen()
+
+  server.printUrls()
 }
--- packages/types/src/cli/index.ts
+++ packages/types/src/cli/index.ts
@@ -1,5 +1,5 @@
 /** @description dev 命令的 action */
-export type ActionDevFunc = (root?: string) => void
+export type ActionDevFunc = (root?: string) => Promise<void>
 
 /** @description build 命令的 action */
 export type ActionBuildFunc = (root?: string) => void
