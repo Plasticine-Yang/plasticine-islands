b6f0c9b3567792184065721e43b4c2132e53e151
plasticine <975036719@qq.com>
2023-03-10 00:11:40 +0800

    feat(types): 添加 cli 包中的 actions 类型

---
+++ packages/types/src/cli/index.ts
@@ -0,0 +1,5 @@
+/** @description dev 命令的 action */
+export type ActionDevFunc = (root?: string) => void
+
+/** @description build 命令的 action */
+export type ActionBuildFunc = (root?: string) => void
