b645b0755d80a3986fe8d66af2d4313c216797f5
yangwenfeng.0719 <yangwenfeng.0719@bytedance.com>
2023-03-12 20:01:09 +0800

    feat(cli): 支持 build 命令

--- packages/cli/src/actions/build.ts
+++ packages/cli/src/actions/build.ts
@@ -1,5 +1,11 @@
+import { resolve } from 'path'
+
+import { build } from '@plasticine-islands/core'
 import type { ActionBuildFunc } from '@plasticine-islands/types'
 
 export const actionBuild: ActionBuildFunc = (root) => {
-  console.log(`build ${root}`)
+  /** @description 需要将相对路径 root 解析成绝对路径，默认使用命令执行时的路径作为 root */
+  const parsedRoot = root !== undefined ? resolve(root) : process.cwd()
+
+  build(parsedRoot)
 }
