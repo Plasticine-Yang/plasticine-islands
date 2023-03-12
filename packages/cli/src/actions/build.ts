3842a54fac3e806f56c4728305568ca159306606
yangwenfeng.0719 <yangwenfeng.0719@bytedance.com>
2023-03-12 21:59:38 +0800

    fix(core): 修复 build 命令 options 默认参数解析问题

--- packages/cli/src/actions/build.ts
+++ packages/cli/src/actions/build.ts
@@ -3,7 +3,7 @@ import { resolve } from 'path'
 import { build } from '@plasticine-islands/core'
 import type { ActionBuildFunc } from '@plasticine-islands/types'
 
-export const actionBuild: ActionBuildFunc = (root, options) => {
+export const actionBuild: ActionBuildFunc = (root, options = {}) => {
   /** @description 需要将相对路径 root 解析成绝对路径，默认使用命令执行时的路径作为 root */
   const parsedRoot = root !== undefined ? resolve(root) : process.cwd()
 
--- packages/core/src/build/index.ts
+++ packages/core/src/build/index.ts
@@ -12,8 +12,9 @@ const defaultBuildCommandOptions: BuildCommandOptions = {
   outdir: CLIENT_BUNDLE_PATH,
 }
 
-export async function build(root: string, options: BuildCommandOptions = defaultBuildCommandOptions) {
-  const [clientBundle] = await bundle(root, options)
+export async function build(root: string, options: BuildCommandOptions) {
+  const resolvedOptions = Object.assign(defaultBuildCommandOptions, options)
+  const [clientBundle] = await bundle(root, resolvedOptions)
 
   // 加载服务端构建产物中的 render 函数
   const serverBundleModulePath = resolve(root, SERVER_BUNDLE_PATH, SERVER_BUNDLE_NAME)
@@ -22,5 +23,5 @@ export async function build(root: string, options: BuildCommandOptions = default
   // 获取客户端产物的入口地址
   const clientEntryChunk = clientBundle.output.find((item) => item.type === 'chunk' && item.isEntry) as OutputChunk
 
-  renderPage(root, render, clientEntryChunk, options)
+  renderPage(root, render, clientEntryChunk, resolvedOptions)
 }
